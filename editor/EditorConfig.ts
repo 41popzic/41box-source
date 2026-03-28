98903f9ac43e6ab685f18d5e08fb8db409d4d1e2
41popzic <superhunter600@gmail.com>
2026-03-28 10:30:23 -0400

    website name 41box 0.2 + patch notes
    i changed the site name to 41box hooray and i also changed the patch notes

--- editor/EditorConfig.ts
+++ editor/EditorConfig.ts
@@ -23,8 +23,8 @@ export function prettyNumber(value: number): string {
 }
 
 export class EditorConfig {
-    public static readonly version: string = "1.4.11"; // Currently using patch versions in display (unlike JB)
-    public static readonly versionDisplayName: string = "Slarmoo's Box " + EditorConfig.version;
+    public static readonly version: string = "0.2"; // Currently using patch versions in display (unlike JB)
+    public static readonly versionDisplayName: string = "41Box " + EditorConfig.version;
 
     public static readonly releaseNotesURL: string = "./patch_notes.html";
 
