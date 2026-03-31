1efa5cce52185213d6114a3901b31dd41b50e9a4
41popzic <superhunter600@gmail.com>
2026-03-31 19:34:06 -0400

    fix url decoding

--- editor/EditorConfig.ts
+++ editor/EditorConfig.ts
@@ -23,7 +23,7 @@ export function prettyNumber(value: number): string {
 }
 
 export class EditorConfig {
-    public static readonly version: string = "0.2.1"; // Currently using patch versions in display (unlike JB)
+    public static readonly version: string = "0.2.1.1"; // Currently using patch versions in display (unlike JB)
     public static readonly versionDisplayName: string = "41Box " + EditorConfig.version;
 
     public static readonly releaseNotesURL: string = "./patch_notes.html";
